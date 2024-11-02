2755d255c66324747f032a3fc66dd6529fa8ac92
Dima <orangefrol@gmail.com>
2024-11-02 13:42:59 +0300

    fix: add search for tm-lambda

--- src/tm.ts
+++ src/tm.ts
@@ -1,30 +1,31 @@
 import { cacheExchange, Client, fetchExchange } from "@urql/core";
 import { retryExchange, RetryExchangeOptions } from "@urql/exchange-retry";
 import { VariablesOf } from "gql.tada";
-import {compact, chain, isEmpty} from 'lodash'
+import { compact, chain, isEmpty } from 'lodash'
 import { tm } from "./providers";
+import { slugify } from "transliteration";
 
 export class TmLambda {
-    client: Client
-    constructor(url: string = "http://tm-lambda.azurewebsites.net/api/graphql") {
-      const retryOptions : RetryExchangeOptions={
-        randomDelay:true,
-        initialDelayMs: 1000,
-        maxDelayMs: 150000,
-        maxNumberAttempts:5,
-        retryIf: ()=> true // always
+  client: Client
+  constructor(url: string = "http://tm-lambda.azurewebsites.net/api/graphql") {
+    const retryOptions: RetryExchangeOptions = {
+      randomDelay: true,
+      initialDelayMs: 1000,
+      maxDelayMs: 150000,
+      maxNumberAttempts: 5,
+      retryIf: () => true // always
     }
-      this.client = new Client({
-        url: url,
-        exchanges: compact([
-          cacheExchange,
-          retryExchange(retryOptions),
-          fetchExchange]),
-      });
-    }
-  
-    async matchesByDate(date:string) {
-        const matchesByDate = tm(`#graphql
+    this.client = new Client({
+      url: url,
+      exchanges: compact([
+        cacheExchange,
+        retryExchange(retryOptions),
+        fetchExchange]),
+    });
+  }
+
+  async matchesByDate(date: string) {
+    const matchesByDate = tm(`#graphql
             query MatchesByDate($date: String!) {
                 getMatchesByDate(date: $date) {
                     id
@@ -49,20 +50,60 @@ export class TmLambda {
             }
         `)
 
-        const params: VariablesOf<typeof matchesByDate> = {
-            date
-        }
-
-      const { data, error } = await this.client.query(matchesByDate, params).toPromise()
-      if (error) {
-        throw new Error(`tm matches request failed: ${error?.toString()}`)
-      }
-      if (!data) {
-        throw new Error(`not matches received`)
-      }
-  
-      return data.getMatchesByDate
+    const params: VariablesOf<typeof matchesByDate> = {
+      date
     }
-  
 
+    const { data, error } = await this.client.query(matchesByDate, params).toPromise()
+    if (error) {
+      throw new Error(`tm matches request failed: ${error?.toString()}`)
+    }
+    if (!data) {
+      throw new Error(`not matches received`)
+    }
+
+    return data.getMatchesByDate
+  }
+
+  async search(text: string) {
+    const searchQuery = tm(`#graphql
+      query Search($text: String!) {
+        search(name: $text){
+          teams{
+            id
+            name
+            countryName
+            competitionName
+            competitionId
+          }
+        }
+      }
+      `)
+
+    const variables: VariablesOf<typeof searchQuery> = {
+        text
+    }
+
+    const { data, error } = await this.client.query(searchQuery, variables).toPromise()
+    if(error){
+      throw new Error(`tm search request failed: ${error?.toString()}`)
+    }
+    if(!data?.search){
+      throw new Error(`not matches received`)
+    }
+
+    return data.search
+    }
+
+}
+
+export type TmTeam={
+  id: string | number,
+  name: string
+}
+
+export function buildTmTeamUrl(tm: TmTeam) {
+  const slug = slugify(tm.name)
+    .replace(/\./g, "") // TODO: also remove punctuation !!!
+  return `https://www.transfermarkt.com/${slug}/startseite/verein/${tm.id}`
 }
